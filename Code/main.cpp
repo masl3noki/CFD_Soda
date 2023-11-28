48f535a0e5e300337df5fd9d94b6a25da973daf3
masl3noki <90714327+masl3noki@users.noreply.github.com>
2023-11-29 00:35:21 +0300

    Рефактор кода
    - Рефактор необходим, т.к. в будущих коммитах будет pdf с теорией и python code.

---
+++ Code/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "main.h"
+
+int main() {
+	std::cout << "Hello!";
+
+	return 0;
+}
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "main.h"
-
-int main() {
-	
-
-	return 0;
-}
